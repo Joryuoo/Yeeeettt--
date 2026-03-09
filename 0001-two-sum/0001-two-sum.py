47eacb88542dbefce54ae232ea6b1572b8590de8
Jhon Ryan Ledon <146564703+Joryuoo@users.noreply.github.com>
2026-03-09 17:05:28 +0800

    Time: 1 ms (61.36%), Space: 13.3 MB (32.32%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        map = {}
+        for i in range(len(nums)):
+            val = target - nums[i]
+            if val in map:
+                return [map[val], i]
+            else:
+                map[nums[i]] = i

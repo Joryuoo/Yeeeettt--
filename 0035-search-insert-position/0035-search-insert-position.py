be332ec69e5c8a4218ff97a5b30b2cde86ca14d9
Jhon Ryan Ledon <146564703+Joryuoo@users.noreply.github.com>
2026-03-22 13:16:53 +0800

    Time: 0 ms (100%), Space: 12.9 MB (84.94%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -6,16 +6,14 @@ class Solution(object):
         :rtype: int
         """
 
-        l, r = 0, len(nums) - 1
-
+        l = 0
+        r = len(nums) - 1
         while l <= r:
-            mid = (l + r) // 2
-            if nums[mid] == target:
-                return mid
-            if target > nums[mid]:
-                l = mid + 1
-            else:
-                r = mid - 1
-
+                mid = (l + r) // 2
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] < target:
+                    l = mid + 1
+                else:
+                    r = mid - 1
         return l
-        
